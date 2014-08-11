09d8421d7b9a04c206020a9acdd63da9e2bbbb04
Eugeny Shevhuk <evgenshe@gmail.com>
2014-08-12 00:06:58 +0300

    project

---
+++ console/migrations/m140723_104509_new_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Schema;
+
+class m140723_104509_new_table extends \yii\db\Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m140723_104509_new_table cannot be reverted.\n";
+
+        return false;
+    }
+}
